d7fcfc56f43ec9b842b73164e3bdb53846602e08
andrianOcto <andrian.octo@gmail.com>
2015-06-27 10:04:09 +0700

    Membuat template Request sementara

---
+++ app/src/main/java/Setting/Server.java
@@ -0,0 +1,11 @@
+package Setting;
+
+/**
+ * Created by octo on 6/27/2015.
+ */
+public class Server {
+    private String path="http://pakarburung.hol.es";
+    public String getPath() {
+        return path;
+    }
+}
